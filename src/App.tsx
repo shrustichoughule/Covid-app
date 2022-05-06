ed38dc031246a53eaeb3c73f69739c1938ddb483
Shrusti Choughule <Shrusti.Choughule@BRILLIO.COM>
2022-05-06 17:21:37 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import Header from "./Components/Header/Header";
+import {  Switch , Route } from 'react-router-dom'
+import AddPatient from './Components/AddPatient/AddPatient'
+import Administrator from "./Components/Administrator/Administrator";
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
+import Home from "./Components/Home/Home";
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:9090/gq",
+  cache: new InMemoryCache()
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Fragment>
+      <div className="container">
+        <Header />
+      </div>
+
+      <div className="container">
+        <Switch>
+            <Route path='/home' exact>
+              <Home></Home>
+            </Route>
+            <Route path='/addpatient'>
+            <ApolloProvider client={client}>
+              <AddPatient />
+              </ApolloProvider>
+            </Route>
+            <Route path='/administrator'>
+            <ApolloProvider client={client}>
+              <Administrator />
+            </ApolloProvider>
+            </Route>
+        </Switch>
+      </div>
+    </Fragment>
+  )
 }
 
 export default App;
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+    return(
+        <div>
+            <br /><br />
+            <h1 className="text-center">Vaccination Application</h1>
+            <p className="text-center">Be CAREFUL</p>
+            <p className="text-center">Be READY to fight</p>
+             <img className="mx-auto d-block" src="https://www.mygov.in/sites/all/themes/mygov/images/covid/block-two.jpg" alt="vaccineimg" width="700" height="400" />
+        </div>
+    )
+}
+export default Home;
